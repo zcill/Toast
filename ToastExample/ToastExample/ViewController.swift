b784d69402a8a0176ec63236381d481d02788570
zcill <zcillcoder@gmail.com>
2016-11-19 02:01:39 +0800

    【Optimize】手写约束，导入Snapkit

--- ToastExample/ToastExample/ViewController.swift
+++ ToastExample/ToastExample/ViewController.swift
@@ -7,9 +7,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var successButton: UIButton!
+    @IBOutlet weak var warningButton: UIButton!
+    @IBOutlet weak var errorButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI() -> Void {
+        
+        successButton.snp.makeConstraints { (make) in
+            make.leading.equalTo(self.view.snp.leading)
+            make.bottom.equalTo(self.view.snp.bottom)
+            make.width.equalTo(self.view.snp.width).dividedBy(3)
+            make.height.equalTo(70)
+        }
+        
+        warningButton.snp.makeConstraints { (make) in
+            make.leading.equalTo(successButton.snp.trailing)
+            make.bottom.equalTo(self.view.snp.bottom)
+            make.size.equalTo(successButton.snp.size)
+        }
+        
+        errorButton.snp.makeConstraints { (make) in
+            make.leading.equalTo(warningButton.snp.trailing)
+            make.bottom.equalTo(self.view.snp.bottom)
+            make.size.equalTo(successButton.snp.size)
+        }
+        
+        
+    }
+    
     @IBAction func showSuccess(_ sender: UIButton) {
         Toast.show(type: .success, message: "设置成功~ ")
     }
