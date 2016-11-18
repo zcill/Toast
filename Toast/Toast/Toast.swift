f035dd62b8427f14254c7b5d877db3389a7e0a40
zcill <zcillcoder@gmail.com>
2016-11-18 18:12:02 +0800

    【Feature】init commit

---
+++ Toast/Toast/Toast.swift
@@ -0,0 +1,21 @@
+//
+//  Toast.swift
+//  Toast
+//
+//  Created by Locke on 2016/11/18.
+//  Copyright © 2016年 Meitu. All rights reserved.
+//
+
+import UIKit
+
+class Toast: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Toast/Toast/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Toast
+//
+//  Created by Locke on 2016/11/18.
+//  Copyright © 2016年 Meitu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func onClicked(_ sender: UIButton) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
